9cffad0a9dcb4d5681242c5881525d698bcc46cf
André Machado <mtz.andremachado@gmail.com>
2022-01-22 04:05:56 -0300

    refactoring models

---
+++ src/app.api/Store.Authorization.API/Models/Response/UserResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿using Store.Authorization.API.Models.User;
+
+namespace Store.Authorization.API.Models.Response
+{
+    public class UserResponseLogin
+    {
+        public string AccesToken { get; set; }
+        public double ExpirationIn { get; set; }
+        public UserToken UserToken { get; set; }
+    }
+}
---
+++ src/app.api/Store.Authorization.API/Models/User/UserJwt.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Store.Authorization.API.Models.User
+{
+
+    public class UserToken
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<UserClaim> Claims { get; set; }
+    }
+
+    public class UserClaim
+    {
+        public string Value { get; set; }
+        public string Type { get; set; }
+    }
+
+}
