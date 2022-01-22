dfb0edc315d26d0c62dca58674c32971f809c0ba
André Machado <mtz.andremachado@gmail.com>
2022-01-22 01:30:00 -0300

    Add project files.

---
+++ src/app.api/Store.Authorization.API/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Store.Authorization.API.Models;
+using System.Threading.Tasks;
+
+namespace Store.Authorization.API.Controllers
+{
+    [Route("api/auth")]
+    public class AuthController : Controller
+    {
+        public readonly SignInManager<IdentityUser> _signInManager;
+        public readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("create-user")]
+        public async Task<IActionResult> CreateUser(UserRegistrationDTO userDTO)
+        {
+            if (ModelState.IsValid == false) return BadRequest();
+
+            var user = new IdentityUser
+            {
+                UserName = userDTO.Email,
+                Email = userDTO.Email,
+                EmailConfirmed = true 
+            };
+
+            var result = await _userManager.CreateAsync(user, userDTO.Password);
+
+            if (result.Succeeded == false)
+            {
+                return BadRequest();
+            }
+
+            await _signInManager.SignInAsync(user, isPersistent: false);
+            return Ok();
+            
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserLoginDTO userDTO)
+        {
+            if (ModelState.IsValid == false) return BadRequest();
+
+            var result = await _signInManager.PasswordSignInAsync(userDTO.Email, userDTO.Password, false, true);
+
+            if (result.Succeeded == false)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ src/app.api/Store.Authorization.API/Models/UserDTO.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Store.Authorization.API.Models
+{
+    public class UserRegistrationDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Compare("Password")]
+        public string ConfirmationPassword { get; set; }
+    }
+
+    public class UserLoginDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
