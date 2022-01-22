d77c32996aba59a1cfa3ceee320bb4d0488596d5
André Machado <mtz.andremachado@gmail.com>
2022-01-22 06:19:26 -0300

    Create Pages Login

---
+++ src/app.web/Store.WebApp.MVC/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.WebApp.MVC.Models.User.Request;
+using System.Threading.Tasks;
+
+namespace Store.WebApp.MVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        [Route("nova-conta")]
+        public async Task<IActionResult> CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> CreateUser(UserRequestDTO userDTO)
+        {
+            if (ModelState.IsValid == false) 
+                return View(userDTO);
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public async Task<IActionResult> Login(UserLoginDTO loginDTO)
+        {
+            if (ModelState.IsValid == false)
+                return View(loginDTO);
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("logout")]
+        public async Task<IActionResult> Logout()
+        {
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ src/app.web/Store.WebApp.MVC/Models/User/Request/UserRequestDTO.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Store.WebApp.MVC.Models.User.Request
+{
+    public class UserRequestDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Compare("Password")]
+        public string ConfirmationPassword { get; set; }
+    }
+
+    public class UserLoginDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
